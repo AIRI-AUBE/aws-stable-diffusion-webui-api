c9a372cca00b22ba0720c81e9c59e1797192dd66
KingLollipop <40582400+jacquesfeng123@users.noreply.github.com>
2023-04-24 00:51:26 +0800

    Update models.py
    try changing options to dict and try again

--- modules/api/models.py
+++ modules/api/models.py
@@ -296,14 +296,14 @@ class InvocationsRequest(BaseModel):
     model: Optional[str]
     vae: Optional[str]
     quality: Optional[int]
-    options: Optional[str]
+    options: Optional[dict]
     txt2img_payload: Optional[StableDiffusionTxt2ImgProcessingAPI]
     img2img_payload: Optional[StableDiffusionImg2ImgProcessingAPI]
     extras_single_payload: Optional[ExtrasSingleImageRequest]
     extras_batch_payload: Optional[ExtrasBatchImagesRequest]
     progress_payload:Optional[ProgressRequest]
     interrogate_payload:Optional[InterrogateRequest]
-    post_options_payload:Optional[]
+    post_options_payload:Optional[dict]
 
 class InvocationsErrorResponse(BaseModel):
     error: str = Field(title="Invocation error", description="Error response from invocation.")
